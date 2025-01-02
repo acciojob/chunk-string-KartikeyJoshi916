8cfd08e1175bb22c698ece04752a5920416a97a4
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2025-01-03 01:11:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 function stringChop(str, size) {
-  // your code here
-}
+	let arr=[];
+	let len=str.length;
+	let count=0;
+	if(len<size){
+		for(let j=0;j<len;j++){
+		subs+=str[count++];
+	}
+	arr.push(subs);
+	return arr;
+	}
+	for(let i=0;i<str.length;i+=size){
+		let subs="";
+		for(let j=0;j<size;j++){
+			subs+=str[count++];
+		}
+		len-=size;
+		arr.push(subs);
+	}
+	let subs="";
+	for(let j=0;j<len;j++){
+		subs+=str[count++];
+	}
+	arr.push(subs);
+	return arr;
+} 
 
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+alert(stringChop(str, size)); 
+  
