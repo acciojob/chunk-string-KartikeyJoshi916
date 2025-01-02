1871bc3b8c0f2490888d580401a212f9a4c947cb
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2025-01-03 01:40:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-function stringChop(str, size) {
-	let arr = [];
-	for(let i = 0; i < str.length; i += size){
-		arr.push(str.substring(i, i + size));
-	}
-	return arr;
-}
+function chunkString(str, chunkLength) {
+    // Return an empty array if the input string is null or the chunk length is invalid
+    if (!str || chunkLength <= 0) return [];
 
+    const result = [];
+    for (let i = 0; i < str.length; i += chunkLength) {
+        result.push(str.slice(i, i + chunkLength));
+    }
+
+    return result;
+}
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size)); 
-  
+   
+
